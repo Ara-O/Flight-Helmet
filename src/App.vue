f9750f6139b67ab61767c2fe7ac86b97cbc4edaf
Ara <oladipoeyiara@gmail.com>
2023-06-27 18:32:15 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { watchEffect, ref } from "vue";
+import renderBurger from "./scripts.ts";
+
+let burgerCanvas = ref<HTMLElement>();
+watchEffect(() => {
+  if (burgerCanvas.value) {
+    renderBurger(burgerCanvas.value);
+  } else {
+    // not mounted yet, or the element was unmounted (e.g. by v-if)
+  }
+});
+</script>
+
+<template>
+  <canvas ref="burgerCanvas" id="burger-canvas"></canvas>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/scripts.ts
@@ -0,0 +1,159 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import * as dat from "lil-gui";
+
+export default function renderBurger(canvas: HTMLElement) {
+  const gltfLoader = new GLTFLoader();
+  const cubeTextureLoader = new THREE.CubeTextureLoader();
+  /**
+   * Base
+   */
+  // Debug
+  const gui = new dat.GUI();
+  const debugObject = { envMapIntensity: 0 };
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  const updateAllMaterials = () => {
+    scene.traverse((child) => {
+      if (
+        child instanceof THREE.Mesh &&
+        child.material instanceof THREE.MeshStandardMaterial
+      ) {
+        // child.material.envMap = environmentMap;
+        child.material.envMapIntensity = debugObject.envMapIntensity;
+      }
+    });
+  };
+
+  //Load environment
+  const environmentMap = cubeTextureLoader.load([
+    "../src/assets/px.jpg",
+    "../src/assets/nx.jpg",
+    "../src/assets/py.jpg",
+    "../src/assets/ny.jpg",
+    "../src/assets/pz.jpg",
+    "../src/assets/nz.jpg",
+  ]);
+
+  scene.background = environmentMap;
+  scene.environment = environmentMap;
+  debugObject.envMapIntensity = 5;
+  gui
+    .add(debugObject, "envMapIntensity")
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name("Env map intensity")
+    .onChange(updateAllMaterials);
+  //   Models
+  gltfLoader.load("../src/assets/FlightHelmet/FlightHelmet.gltf", (gltf) => {
+    gltf.scene.scale.set(10, 10, 10);
+    gltf.scene.position.set(0, -4, 0);
+    gltf.scene.rotation.y = Math.PI * 0.5;
+    scene.add(gltf.scene);
+    gui
+      .add(gltf.scene.rotation, "y")
+      .min(-Math.PI)
+      .max(Math.PI)
+      .step(0.001)
+      .name("Rotation");
+  });
+
+  //   Light
+  const directionalLight = new THREE.DirectionalLight();
+  directionalLight.position.set(0.25, 3, -2.25);
+  scene.add(directionalLight);
+
+  gui
+    .add(directionalLight, "intensity")
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .name("Light intensity");
+
+  gui
+    .add(directionalLight.position, "x")
+    .min(-5)
+    .max(5)
+    .step(0.001)
+    .name("Light intensity X");
+  gui
+    .add(directionalLight.position, "y")
+    .min(-5)
+    .max(5)
+    .step(0.001)
+    .name("Light intensity Y");
+  gui
+    .add(directionalLight.position, "z")
+    .min(-5)
+    .max(5)
+    .step(0.001)
+    .name("Light intensity Z");
+
+  /**
+   * Sizes
+   */
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
+
+  /**
+   * Camera
+   */
+  // Base camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
+  );
+  camera.position.set(4, 1, -4);
+  scene.add(camera);
+
+  // Controls
+  const controls = new OrbitControls(camera, canvas);
+  controls.enableDamping = true;
+
+  /**
+   * Renderer
+   */
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+  });
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  /**
+   * Animate
+   */
+  const tick = () => {
+    // Update controls
+    controls.update();
+
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+  };
+
+  tick();
+}
